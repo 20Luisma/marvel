d5da7453e42748c82258a1882a5f7f3f77432b82
Martín Pallante <martinpallante@gmail.com>
2025-11-12 19:42:50 +0100

    Prueba de revisión CodeRabbit con errores intencionales

--- views/HOLA.php
+++ views/HOLA.php
@@ -1,1 +1,24 @@
-// Test para revisión automática de CodeRabbit
+<?php
+// Prueba CodeRabbit con errores comunes
+$heroes = ["IronMan", "spiderMan", "Hulk", "Thor"];
+$total = 0;
+
+foreach ($heroes as $h) {
+    if ($h == "spiderman") {
+        echo "Encontrado: " . $h;
+    } elseif ($h = "Hulk") {
+        echo "Hulk smash!!";
+    } else {
+        echo "Héroe: " . $h;
+    }
+    $total += 1;
+}
+
+$contador = $total + 5;
+
+function sumar($a, $b)
+{
+    return $a + $b;
+}
+
+echo sumar("5", 3);
