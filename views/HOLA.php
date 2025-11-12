b17ef9c0a13211ca76c9251c370903273ddcfddb
Martín Pallante <martinpallante@gmail.com>
2025-11-12 19:29:55 +0100

    Pull Request de prueba para CodeRabbit

--- views/HOLA.php
+++ views/HOLA.php
@@ -1,11 +1,1 @@
-<?php
-// Probando CodeRabbit con algo de lógica
-$heroes = ["IronMan", "SpiderMan", "Hulk", "Thor"];
-
-foreach ($heroes as $h) {
-    if ($h == "Hulk") {
-        echo "Hulk smash!";
-    } else {
-        echo "Hello " . $h;
-    }
-}
+// Test para revisión automática de CodeRabbit
