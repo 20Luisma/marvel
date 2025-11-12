0886f93a00069a02685f06688e62de3d6cd474c4
Martín Pallante <martinpallante@gmail.com>
2025-11-12 19:27:29 +0100

    sync desde local

--- views/HOLA.php
+++ views/HOLA.php
@@ -1,1 +1,11 @@
-// forzar detección de coderabbit.yaml
+<?php
+// Probando CodeRabbit con algo de lógica
+$heroes = ["IronMan", "SpiderMan", "Hulk", "Thor"];
+
+foreach ($heroes as $h) {
+    if ($h == "Hulk") {
+        echo "Hulk smash!";
+    } else {
+        echo "Hello " . $h;
+    }
+}
